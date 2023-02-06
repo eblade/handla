0f539f7f5c84eb53f5e05c4a239e22639e7ff55c
Johan Egneblad <johan@egneblad.se>
2023-02-06 19:34:32 +0100

    Add a lock to the db class

--- handlapy/db.py
+++ handlapy/db.py
@@ -1,38 +1,43 @@
 import sqlite3
 import os
+import threading
 from contextlib import closing
 
 
 class Database:
     def __init__(self, path: str):
         self.path = path
+        self.lock = threading.Lock()
         self.setup()
 
     def connect(self):
         return sqlite3.connect(self.path)
 
     def setup(self):
-        with closing(self.connect()) as con:
-            with closing(con.cursor()) as cur:
-                cur.execute("CREATE TABLE IF NOT EXISTS item(name, category, state INTEGER, comment)")
-            con.commit()
+        with self.lock:
+            with closing(self.connect()) as con:
+                with closing(con.cursor()) as cur:
+                    cur.execute("CREATE TABLE IF NOT EXISTS item(name, category, state INTEGER, comment)")
+                con.commit()
 
     def select(self, factory):
-        with closing(self.connect()) as con:
-            with closing(con.cursor()) as cur:
-                 yield from (factory(*row) for row in cur.execute("SELECT * FROM item"))
+        with self.lock:
+            with closing(self.connect()) as con:
+                with closing(con.cursor()) as cur:
+                     yield from (factory(*row) for row in cur.execute("SELECT * FROM item"))
 
     def upsert(self, old_name: str, old_category: str, name: str, category: str, state: int, comment: str):
-        with closing(self.connect()) as con:
-            with closing(con.cursor()) as cur:
-                params = (old_name, old_category)
-                res = cur.execute("SELECT * FROM item WHERE name = ? AND category = ?", params)
-                existing = res.fetchone()
-                if existing is None:
-                    self.insert(cur, name, category, state, comment)
-                else:
-                    self.update(cur, old_name, old_category, name, category, state, comment)
-            con.commit()
+        with self.lock:
+            with closing(self.connect()) as con:
+                with closing(con.cursor()) as cur:
+                    params = (old_name, old_category)
+                    res = cur.execute("SELECT * FROM item WHERE name = ? AND category = ?", params)
+                    existing = res.fetchone()
+                    if existing is None:
+                        self.insert(cur, name, category, state, comment)
+                    else:
+                        self.update(cur, old_name, old_category, name, category, state, comment)
+                con.commit()
 
     def insert(self, cur, name: str, category: str, state: int, comment: str):
         params = dict(name=name, category=category, state=state, comment=comment)
@@ -43,9 +48,10 @@ class Database:
         res = cur.execute("UPDATE item SET name = ?, category = ?, state = ?, comment = ? WHERE name = ? AND category = ?", params)
 
     def delete(self, name: str, category: str):
-        with closing(self.connect()) as con:
-            with closing(con.cursor()) as cur:
-                params = (name, category)
-                res = cur.execute("DELETE FROM item WHERE name = ? AND category = ?", params)
-            con.commit()
+        with self.lock:
+            with closing(self.connect()) as con:
+                with closing(con.cursor()) as cur:
+                    params = (name, category)
+                    res = cur.execute("DELETE FROM item WHERE name = ? AND category = ?", params)
+                con.commit()
 
