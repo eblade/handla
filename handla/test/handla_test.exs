ae15f5b29fc0023f1e08fba72ec6188c6dcfb1a0
Johan Egneblad <johan@egneblad.se>
2023-02-14 07:25:14 +0100

    Ta bort elixiransatsen

--- handla/test/handla_test.exs
+++
@@ -1,8 +0,0 @@
-defmodule HandlaTest do
-  use ExUnit.Case
-  doctest Handla
-
-  test "greets the world" do
-    assert Handla.hello() == :world
-  end
-end
--- handla/test/test_helper.exs
+++
@@ -1,3 +0,0 @@
-Code.require_file "support/builder.exs", __DIR__
-
-ExUnit.start()
